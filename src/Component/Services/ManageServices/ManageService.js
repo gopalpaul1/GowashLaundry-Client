80cc03eac0b98fc21e1b79f827f376c9e50b9eb7
Gopal Paul <gopal.kumar.paul1@gmail.com>
2021-04-18 16:47:57 +0600

    client-side

--- src/Component/Services/ManageServices/ManageService.js
+++ src/Component/Services/ManageServices/ManageService.js
@@ -9,7 +9,7 @@ const ManageService = () => {
 
     useEffect(() => {
 
-        fetch('http://localhost:5055/services')
+        fetch('https://blooming-sea-02282.herokuapp.com/services')
             .then(res => res.json())
             .then(data => setManageService(data))
 
